67d1f81db72feefd79a5a91c57167bcabecba0bb
RenSyntax <ren.syntax@gmail.com>
2026-08-04 22:15:18 +0530

    feat(stl): code for unordered set

---
+++ 01_cpp_fundamentals/standard_templete_library/unordered_set.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void unordered_set() {
+    // Its similar to set, but only Unique, not ordered.
+    unorderedset <int> ust;
+
+    // all operations are similar to set but lower bound and upper bound function does not work.
+    // all operations are mostly in o(1) time complexity.
+}
